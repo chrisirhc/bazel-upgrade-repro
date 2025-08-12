d1cae65ee944d022da723065513d5e62d045bd23
chua <chua@uber.com>
2025-08-12 11:11:15 +0000

    add some comments for readability

--- reproduce.sh
+++ reproduce.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Start with Bazel 7.4.1
-bazel run //:my_script
+bazel run //:my_script # Successful
 echo "8.3.1" > .bazelversion
-bazel run //:my_script
+bazel run //:my_script # Successful
 echo "7.4.1" > .bazelversion
-bazel run //:my_script
+bazel run //:my_script # Fails
